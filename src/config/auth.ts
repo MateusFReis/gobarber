9c447f9705a3155923dc44f5f78fc4c7f8b6a7e3
Mateus Reis <ranmateus@gmail.com>
2020-06-11 11:55:26 -0300

    Arquitetura e Testes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a7a8c52699ad2a11662884a669d93e17',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
+// import { uuid } from 'uuidv4';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  date: Date;
+}
+
+@injectable()
+class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      // id: uuid(),
+      provider_id,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
